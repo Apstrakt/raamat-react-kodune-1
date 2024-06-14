0641afb6bf3890b0f599e29bb6fba092b42fc2df
Hando <hando.riispapp@gmail.com>
2024-06-14 13:57:55 +0300

    d

--- src/AddNewBook.js
+++ src/AddNewBook.js
@@ -1,71 +1,60 @@
-import { useState } from 'react'
-import pipi from './covers/Pipi.jpg'
+import { useState } from "react";
 
-function AddNewBook({addBook}) {
-    const [title, setTitle] = useState('')
-    const [author, setAuthor] = useState('')
-    const [genre, setGenre] = useState('')
-    const [picture, setPicture] = useState('')
-    const submitValues = () => {
-        console.log("About to submit values ");
-        console.log('title', title)
-        let userPicture = "https://d33318exxque5k.cloudfront.net/covers/eesti/large/9789985576175.jpg";
-        
-         if (picture) {
-            userPicture = picture
-        } 
+function AddNewBook({ addBook }) {
+  const [title, setTitle] = useState("");
+  const [author, setAuthor] = useState("");
+  const [genre, setGenre] = useState("");
+  const [picture, setPicture] = useState("");
+  const submitValues = () => {
+    console.log("About to submit values ");
+    console.log("title", title);
+    let userPicture =
+      "https://d33318exxque5k.cloudfront.net/covers/eesti/large/9789985576175.jpg";
 
-    
-        
-        addBook({ title, author, genre, picture:userPicture })
-        setTitle('')
-        setAuthor('')
-        setGenre('')
-        setPicture('')
+    if (picture) {
+      userPicture = picture;
     }
 
-    return (
+    addBook({ title, author, genre, picture: userPicture });
+    setTitle("");
+    setAuthor("");
+    setGenre("");
+    setPicture("");
+  };
 
-        <>
-        <div className="container row clickable">
-            <input 
-                className="col-12 col-sm-12" 
-                placeholder="Book title"
-                value={title}
-                onChange={(event) => setTitle(event.target.value) }
-            />
-            <input 
-                className="col-12" 
-                placeholder="Author"
-                value={author}
-                onChange={(event) => setAuthor(event.target.value) }
-            />
-            <input 
-                className="col-12" 
-                placeholder="Genre"
-                value={genre}
-                onChange={(event) => setGenre(event.target.value) }
-            />
-            <input 
-                className="col-12" 
-                placeholder="URL"
-                value={picture}
-                onChange={(event) => setPicture(event.target.value) }
-            />
-            <button 
-                className='col-12 '
-                onClick={submitValues}
-            >
-                Add
-            </button>
-
-        </div>
-        </>
-    )
+  return (
+    <>
+      <div className="container row clickable">
+        <input
+          className="col-12 col-sm-12"
+          placeholder="Book title"
+          value={title}
+          onChange={(event) => setTitle(event.target.value)}
+        />
+        <input
+          className="col-12"
+          placeholder="Author"
+          value={author}
+          onChange={(event) => setAuthor(event.target.value)}
+        />
+        <input
+          className="col-12"
+          placeholder="Genre"
+          value={genre}
+          onChange={(event) => setGenre(event.target.value)}
+        />
+        <input
+          className="col-12"
+          placeholder="URL"
+          value={picture}
+          onChange={(event) => setPicture(event.target.value)}
+        />
+        <button className="col-12 " onClick={submitValues}>
+          Add
+        </button>
+      </div>
+    </>
+  );
 }
 
-export default AddNewBook
-        
-
-    
-   
+export default AddNewBook;
