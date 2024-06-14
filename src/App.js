7028230cdcb70035652cc1c02acb1f86add86615
Hando <hando.riispapp@gmail.com>
2024-06-14 14:07:02 +0300

    eslint-disable

--- src/App.js
+++ src/App.js
@@ -1,135 +1,91 @@
-import './App.css';
-import Book from './Book.js';
-import Book2 from './Book2.js';
-import natr from './covers/Naksitrallid.jpg'
-import pipi from './covers/Pipi.jpg'
-import gof from './covers/GobletOfFire.jpg'
-import ReadCount from './ReadCount.js';
-import AddNewBook from './AddNewBook.js';
-import { useState } from 'react';
+import "./App.css";
+import Book from "./Book.js";
+import Book2 from "./Book2.js";
+eslint-disable-next-line no-unused-vars
+import natr from "./covers/Naksitrallid.jpg";
+eslint-disable-next-line no-unused-vars
+import pipi from "./covers/Pipi.jpg";
+eslint-disable-next-line no-unused-vars
+import gof from "./covers/GobletOfFire.jpg";
+
+import AddNewBook from "./AddNewBook.js";
+import { useState } from "react";
 
 function App() {
   const [books, setBooks] = useState([
     {
-      title: 'Pipi Pikksukk',
-      author: 'Astrid Lindgren',
-      genre: 'Children`s book, Adventure',
+      title: "Pipi Pikksukk",
+      author: "Astrid Lindgren",
+      genre: "Children`s book, Adventure",
       picture: pipi,
-    
     },
     {
-      title: ' Jälle need Naksitrallid',
-      author: 'Eno Raud',
-      genre: 'Children`s book, Adventure',
+      title: " Jälle need Naksitrallid",
+      author: "Eno Raud",
+      genre: "Children`s book, Adventure",
       picture: natr,
-    
     },
     {
-      title: 'Harry Potter and the Goblet of Fire',
-      author: 'J. K. Rowling',
-      genre: 'Fantasy',
-      picture: gof
-    }
+      title: "Harry Potter and the Goblet of Fire",
+      author: "J. K. Rowling",
+      genre: "Fantasy",
+      picture: gof,
+    },
   ]);
 
   const [selectedBook, setSelectedBook] = useState(null);
 
   const addBook = ({ title, author, genre, picture }) => {
-    console.log('About to add ' + title)
+    console.log("About to add " + title);
     const newBook = {
       title,
       author,
       genre,
       picture,
     };
-    
-    setBooks([...books, newBook])
+
+    setBooks([...books, newBook]);
   };
-    
-    const handleClick = (book) => {
-      setSelectedBook(book);
-    };
-return (
- 
- <div className="container">
-   <h1 className="Heading">The Library</h1>
-   <div className="leftPanel AppL col-6 ">
-      <div classname="addBooks">
-        <h4 className="clickable">Add a new book to the library</h4>
-        <AddNewBook addBook={addBook} />
+
+  const handleClick = (book) => {
+    setSelectedBook(book);
+  };
+  return (
+    <div className="container">
+      <h1 className="Heading">The Library</h1>
+      <div className="leftPanel AppL col-6 ">
+        <div classname="addBooks">
+          <h4 className="clickable">Add a new book to the library</h4>
+          <AddNewBook addBook={addBook} />
+        </div>
+        <div className="clickable">
+          {books.map((book, index) => (
+            <div
+              key={index}
+              onClick={() => handleClick(book)}
+              style={{ cursor: "pointer" }}
+            >
+              <Book booksReal={book} />
+            </div>
+          ))}
+        </div>
       </div>
-      <div className="clickable">
-          {
-            books.map((book, index) => (
-              <div key={index} onClick={() => handleClick(book)} style={{ cursor: 'pointer' }}>
-                <Book booksReal={book} />
-              </div>
-            ))
-          }
-      </div>
-   </div>
-    <div className="rightPanel AppR col-6">
-      {selectedBook ? (
+      <div className="rightPanel AppR col-6">
+        {selectedBook ? (
           <Book2
-          title={selectedBook.title}
-          author={selectedBook.author}
-          genre={selectedBook.genre}
-          picture={selectedBook.picture}
+            title={selectedBook.title}
+            author={selectedBook.author}
+            genre={selectedBook.genre}
+            picture={selectedBook.picture}
           />
         ) : (
-          <div className="askForDetails"><h4>Select a book to see details</h4></div>
+          <div className="askForDetails">
+            <h4>Select a book to see details</h4>
+          </div>
         )}
-   </div>
- </div>
-);
+      </div>
+    </div>
+  );
 }
-   
-
-
-          
-     
-
-
-
-  
-
-
-    
-
- 
-  
-
-     
-     
-        
-  
-
-
-   
-   
-
-
-        
-          
-        
-      
-
-
-
-        
-       
-       
-        
-
-
-       
-          
-      
-        
-        
-        
-      
 
 export default App;
-      
- 
--- src/Book.js
+++ src/Book.js
@@ -1,50 +1,12 @@
-import ReadCount from './ReadCount.js'
-import { useState } from 'react';
+import ReadCount from "./ReadCount.js";
+import { useState } from "react";
 
-function Book({booksReal}) {
-    
-        let bookStyle = ""
-        let genreStyle = ""
-        let authorStyle = ""
-        
- 
-  if (booksReal.title) {
-      bookStyle = "books"
-  }
-
-  if (booksReal.author) {
-      authorStyle = "authors"
-  }
-
-  if (booksReal.genre) {
-     genreStyle = "genres"
-  }
+function Book({ booksReal }) {
   return (
-      < div >
-          <h4 className="books">{booksReal.title}</h4>
-         
-          
-          
-          
-      </div>
-  )
+    <div>
+      <h4 className="books">{booksReal.title}</h4>
+    </div>
+  );
 }
 
-  
-
-       
-   
-  
-  
-    
-
-
-
-  
-  
-
-    
-        
-           
-
-export default Book
+export default Book;
